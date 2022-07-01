4ca81bedb2532f201c6ca34b27809c4a8de5b924
CodeNextCoachPaul <108353724+CodeNextCoachPaul@users.noreply.github.com>
2022-07-01 11:22:00 -0400

    Update consoles.py
    Still an error "NameError: name 'priceOne' is not defined", decided to just use print statements and hard coded inputs, bugfixing still

--- consoles.py
+++ consoles.py
@@ -3,22 +3,35 @@ class ps5game:
         self.gameOne = gameOne
         self.gameTwo = gameTwo
         self.priceOne = priceOne
-        self.priceOne = priceOne
+        self.priceTwo = priceTwo
+    def printOptions(self):
+        print(f"Would you like to purchase {self.gameOne} - ${priceOne} and {self.gameTwo} ${priceTwo}?")
 class ps4game:
     def __init__(self,gameOne,priceOne,gameTwo,priceTwo):
         self.gameOne = gameOne
         self.gameTwo = gameTwo
         self.priceOne = priceOne
-        self.priceOne = priceOne
+        self.priceTwo = priceTwo
+    def printOptions(self):
+        print(f"Would you like to purchase {self.gameOne} - ${priceOne} and {self.gameTwo} ${priceTwo}?")
 class xb1game:
     def __init__(self,gameOne,priceOne,gameTwo,priceTwo):
         self.gameOne = gameOne
         self.gameTwo = gameTwo
         self.priceOne = priceOne
-        self.priceOne = priceOne
+        self.priceTwo = priceTwo
+    def printOptions(self):
+        print(f"Would you like to purchase {self.gameOne} - ${priceOne} and {self.gameTwo} ${priceTwo}?")
 class xb1xgame:
     def __init__(self,gameOne,priceOne,gameTwo,priceTwo):
         self.gameOne = gameOne
         self.gameTwo = gameTwo
         self.priceOne = priceOne
-        self.priceOne = priceOne
+        self.priceTwo = priceTwo
+    def printOptions(self):
+        print(f"Would you like to purchase {self.gameOne} - ${priceOne} and {self.gameTwo} ${priceTwo}?")
+
+ps5items = ps5game("Resident Evil: Village", 70, "Batman Arkham Collection", 70)
+ps4items = ps4game("Street Fighter 5", 40, "Black Ops 3", 25)
+xb1items = xb1game("Infinite Warfare", 15, "2K21", 20)
+xb1xitems = xb1xgame("Gotham Knights", 70, "MK11", 36)
