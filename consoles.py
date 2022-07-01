939f21a519aa07433c61dccb3ba63a952c307360
CodeNextCoachPaul <108353724+CodeNextCoachPaul@users.noreply.github.com>
2022-07-01 10:28:32 -0400

    Update consoles.py
    added classes for each console since each different console/section has different games and those diff games have diff prices, going to fix class implementation now

---
+++ consoles.py
@@ -0,0 +1,24 @@
+class ps5game:
+    def __init__(self,gameOne,priceOne,gameTwo,priceTwo):
+        self.gameOne = gameOne
+        self.gameTwo = gameTwo
+        self.priceOne = priceOne
+        self.priceOne = priceOne
+class ps4game:
+    def __init__(self,gameOne,priceOne,gameTwo,priceTwo):
+        self.gameOne = gameOne
+        self.gameTwo = gameTwo
+        self.priceOne = priceOne
+        self.priceOne = priceOne
+class xb1game:
+    def __init__(self,gameOne,priceOne,gameTwo,priceTwo):
+        self.gameOne = gameOne
+        self.gameTwo = gameTwo
+        self.priceOne = priceOne
+        self.priceOne = priceOne
+class xb1xgame:
+    def __init__(self,gameOne,priceOne,gameTwo,priceTwo):
+        self.gameOne = gameOne
+        self.gameTwo = gameTwo
+        self.priceOne = priceOne
+        self.priceOne = priceOne
